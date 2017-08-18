03b51b05eab06013de70fc3c7cd042c06ce2b774
Rob <r.indesteege@gmail.com>
2017-08-18 18:18:38 +0200

    change webpack config, to fix problem on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   entry: './src/index.js',
 
   output: {
-    path: path.resolve('dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
     publicPath: '/'
   },
