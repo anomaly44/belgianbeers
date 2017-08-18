61eddf489e411b94a1ad82f43c9265018cbb6128
Rob <r.indesteege@gmail.com>
2017-08-18 14:24:02 +0200

    add some styling, add required info

--- src/containers/BeerDetail/index.js
+++ src/containers/BeerDetail/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Card, CardTitle, CardText } from 'reactstrap'
+import StarRating from '../StarRating'
 
 export default class BeerDetail extends Component {
 
@@ -12,13 +14,38 @@ export default class BeerDetail extends Component {
     return beers[beers.findIndex(beer => beer.id === id)];
   };
 
-  render () {
+  render() {
     const beer = this.getSelectedBeer();
-
+    const brewery = beer.brewery;
     return (
       <div className="container">
-        <h2>{beer.name}</h2>
-    </div>)
+        <h2 style={{ marginTop: 20, marginBottom: 20 }}>
+          {beer.name}
+        </h2>
+        <div className="row">
+          <div className="col-xs-12 col-md-6" style={{textAlign: 'center'}}>
+            <img src={beer.image_url} alt={beer.name}/>
+          </div>
+          { brewery && <div className="col-xs-12 col-md-6">
+            <Card style={{ padding: 20 }}>
+              <CardTitle>
+                {brewery.name}
+              </CardTitle>
+              <CardText>
+                {brewery.address} <br/>
+                {brewery.city}
+              </CardText>
+            </Card>
+            <div>
+              <h3 style={{ marginTop: 20, marginBottom: 20 }}>
+                {beer.rating ? 'Change the rating of this beer:' : 'Rate this beer'}
+              </h3>
+              <StarRating value={beer.rating} editing large />
+            </div>
+          </div>}
+        </div>
+
+      </div>)
   }
 }
 
