0ff66e8074f09931a85fa36557df01bf0a33ab1e
Rob <r.indesteege@gmail.com>
2017-08-20 11:29:45 +0200

    add sort and separate the beersListItem from beersList

---
+++ src/components/BeersListItem.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+// # Components
+import StarRating from './StarRating'
+import H2 from './H2'
+
+const styles = {
+  beerContainer: {
+    border: '1px solid #B6B6B6',
+    borderRadius: 5,
+    padding: 20,
+    height: '100%'
+  },
+  listItem: {
+    padding: 5
+  },
+};
+
+export default function BeersListItem ({beer}) {
+  return (
+    <div
+      className="col-xs-12 col-md-6 col-xl-4"
+      style={styles.listItem}
+    >
+      <div style={styles.beerContainer}>
+        <div className="row">
+          <div className="col-xs-3">
+            <img src={beer.thumb_image_url} alt={beer.name} height={100} width={100}/>
+          </div>
+          <div className="col-xs-7 col-md-7 offset-1">
+            <H2><Link to={`/beers/${beer.id}`}>{beer.name}</Link></H2>
+            {beer.brewery && <p>{beer.brewery.name}</p>}
+            { beer.rating ?
+              <StarRating editing={false} value={beer.rating}/> :
+              <p>Not rated yet.</p>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+BeersListItem.propTypes = {
+  beer: PropTypes.object.isRequired
+};
+
--- src/pages/BeersList.js
+++ src/pages/BeersList.js
@@ -2,62 +2,44 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {
   Input,
-  Label,
-  FormGroup
+  FormGroup,
+  Form
 } from 'reactstrap'
-import { Link } from 'react-router-dom'
 
 // # Components
-import StarRating from '../components/StarRating'
 import H1 from '../components/H1'
-import H2 from '../components/H2'
+import BeersListItem from '../components/BeersListItem'
+
 
 const styles = {
   container: {
     marginTop: 30,
     marginBottom: 30,
   },
-  listItem: {
-    padding: 5
+  filterSelect: {
+    display: 'inline-block',
+    marginRight: 10 
   },
-  beerContainer: {
-    border: '1px solid #B6B6B6',
-    borderRadius: 5,
-    padding: 20
+  sortSelect: {
+    display: 'inline-block',
+    marginLeft: 10
   }
 };
-
-const renderBeer = (beer) => (
-  <div
-    key={beer.id}
-    className="col-xs-12 col-md-6 col-xl-4"
-    style={styles.listItem}
-  >
-    <div style={styles.beerContainer}>
-      <div className="row">
-        <div className="col-xs-3">
-          <img src={beer.thumb_image_url} alt={beer.name} height={100} width={100}/>
-        </div>
-        <div className="col-xs-7 offset-1">
-          <H2><Link to={`/beers/${beer.id}`}>{beer.name}</Link></H2>
-          { beer.rating ?
-            <StarRating editing={false} value={beer.rating}/> :
-            <p>This beer is not rated yet.</p>}
-        </div>
-      </div>
-    </div>
-  </div>
-);
 
 
 export default class BeersList extends Component {
 
   state = {
-    selectFilterValue: 0
+    selectFilterValue: 0,
+    selectSortValue: 'alphabet'
   };
 
   handleFilterChange = (event) => {
     this.setState({ selectFilterValue: event.target.value * 1 });
+  };
+
+  handleSortChange = (event) => {
+    this.setState({ selectSortValue: event.target.value });
   };
 
   filterBeerIfNecessary = (beer) => {
@@ -68,36 +50,64 @@ export default class BeersList extends Component {
     }
 
     return !!beer.rating;
+
+  };
+
+  sortBeers = (beerA, beerB) => {
+    const { selectSortValue } = this.state;
+
+    // when sorting alphabetically:
+    if (selectSortValue === 'alphabet') {
+      return beerA.name > beerB.name ? 1 : -1;
+    }
+
+    // when sorting by rating
+    return (beerB.rating || 0) - (beerA.rating || 0);
+
   };
 
   render() {
     const { beers } = this.props;
-    const { selectFilterValue } = this.state;
+    const { selectFilterValue, selectSortValue } = this.state;
 
     return (
       <div className="container" style={styles.container}>
-        <div className="row">
-          <div className="col-xs-12">
+        <div className="row justify-content-md-center justify-content-xs-center">
+          <div className="col-xs-12" style={{textAlign: 'center'}}>
             <H1>Belgian Beers</H1>
-            <FormGroup>
-              <Label for="selectFilter">Show:</Label>
-              <Input
-                type="select"
-                name="selectFilter"
-                id="selectFilter"
-                value={selectFilterValue}
-                onChange={this.handleFilterChange}
-              >
-                <option value={0}>All beers</option>
-                <option value={1}>Only rated beers</option>
-              </Input>
-            </FormGroup>
+            <Form>
+              <FormGroup style={styles.filterSelect}>
+                <Input
+                  type="select"
+                  name="selectFilter"
+                  id="selectFilter"
+                  value={selectFilterValue}
+                  onChange={this.handleFilterChange}
+                >
+                  <option value={0}>All beers</option>
+                  <option value={1}>Only rated beers</option>
+                </Input>
+              </FormGroup>
+              <FormGroup style={{ display: 'inline-block', marginLeft: 10 }}>
+                <Input
+                  type="select"
+                  name="selectSort"
+                  id="selectSort"
+                  value={selectSortValue}
+                  onChange={this.handleSortChange}
+                >
+                  <option value="alphabet">Alphabetically</option>
+                  <option value="rating">By rating</option>
+                </Input>
+              </FormGroup>
+            </Form>
           </div>
         </div>
         <div className="row">
           {beers
             .filter(this.filterBeerIfNecessary)
-            .map(renderBeer)}
+            .sort(this.sortBeers)
+            .map(beer => <BeersListItem beer={beer} key={beer.id} />)}
         </div>
       </div>
     );
